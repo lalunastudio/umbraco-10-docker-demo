e54d49eacc7460a8d4521fb2f6dc567b3823741a
Peter Nielsen <peter@laluna.io>
2022-06-26 14:55:46 +0200

    Updated settings to run with "Url": "http://0.0.0.0:4000"

--- localdocker.sh
+++ localdocker.sh
@@ -1,2 +1,2 @@
 docker build --no-cache --progress=plain -t umbraco10test_docker --build-arg PROJECT=Umbraco10Test2 .
-docker run --rm -d -p 80:80 -p 443:443 --name umbraco10 umbraco10test_docker
+docker run --rm -d -p 4000:4000 --name umbraco10 umbraco10test_docker
